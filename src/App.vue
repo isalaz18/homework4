5671c79e0fa905807071b86692dfb933ec942172
Italia Salazar <isalaz18@stu.southtexascollege.edu>
2025-09-28 17:29:52 -0500

    finished income expense compnent

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import {ref, computed} from 'vue'
-
+import IncomeExpense from './components/IncomeExpense.vue';
 
 const transactions = ref([
 {id: 1, text:'Paycheck', amount: 700.00},
@@ -17,12 +17,30 @@ const sum = computed(()=>{
   },0)
 })
 
+const moneyIn = computed(()=>{
+  return transactions.value
+  .filter((x)=>x.amount>0)
+  .reduce((acc, x)=>{
+    return acc+x.amount
+  },0)
+})
+
+const moneyOut = computed(()=>{
+  return transactions.value
+  .filter((x)=>x.amount<0)
+  .reduce((acc, x)=>{
+    return acc+x.amount
+  },0)
+})
+
+
 </script>
 
 <template>
 <Header></Header>
 <div class="container">
-<Balance :total="sum"></Balance>
+  <Balance :total="sum"></Balance>
+  <IncomeExpense :income ="moneyIn" :expense="-moneyOut"></IncomeExpense>
 </div>
 
 
